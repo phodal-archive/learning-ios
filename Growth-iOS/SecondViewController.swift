9fb81972f13e1c5cacb1c1dc51c4b76b3f152238
Fengda HUANG <h@phodal.com>
2016-09-19 07:24:29 +0800

    Initial Commit

---
+++ Growth-iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Growth-iOS
+//
+//  Created by Fengda Huang on 9/19/16.
+//  Copyright © 2016 Fengda Huang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
